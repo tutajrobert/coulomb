415dc404cad18ed517d3ffaf9610a513a7cf0a0b
tutaj robert <tutajrobert@gmail.com>
2017-03-28 09:59:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ ws.style('main.css')
 
 ws.title('GRoT> Graphical Resolver of T4s')
 
-ws.header('Graphical Resolver of T4s',
-          'View the project on GitHub: <a href="https://github.com/tutajrobert/grot"> tutajrobert/grot</a>&nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;Correspond with the author: <a href="mailto:tutajrobert@gmail.com">tutajrobert</a>')
-
+ws.header('GRAPHICAL RESOLVER of T4s',
+          'View the project on GitHub: <a href="https://github.com/tutajrobert/grot"> tutajrobert/grot</a>&nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;Correspond with the author: <a href="mailto:tutajrobert@gmail.com">tutajrobert</a>')	  
+		  
 ws.menu([('main.html', 'Main'), 
          ('inst.html', 'Installation'), 
          ('manual.html', 'Manual'), 
@@ -21,5 +21,20 @@ ws.footer('Project is maintained by <a href="https://github.com/tutajrobert">tut
 
 ws.page('main.html',
        open('grot_site' + os.sep + 'main.txt', 'r').read())
+	   
+ws.page('inst.html',
+       open('grot_site' + os.sep + 'inst.txt', 'r').read())
+	   
+ws.page('manual.html',
+       open('grot_site' + os.sep + 'manual.txt', 'r').read())
+	   
+ws.page('examples.html',
+       open('grot_site' + os.sep + 'examples.txt', 'r').read())
+	   
+ws.page('tutorial.html',
+       open('grot_site' + os.sep + 'tutorial.txt', 'r').read())
+	   
+ws.page('struct.html',
+       open('grot_site' + os.sep + 'struct.txt', 'r').read())
 
 ws.publish()
